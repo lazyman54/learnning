d180c0ef8863056bee7f8cb2fab1efb2ed02aa07
lazyman <lazymank54@gmail.com>
2017-09-14 15:55:05 +0800

    thread local problem

--- src/main/java/com/ek/study/conCurrent/ThreadLocalStudy_alone.java
+++ src/main/java/com/ek/study/conCurrent/ThreadLocalStudy_alone.java
@@ -3,26 +3,20 @@ package com.ek.study.conCurrent;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
+ * ThreadLocal是每个线程独有的
+ *
  * @author lazyman
  * @version v1.0
  * @date 2017/6/21
  */
-public class ThreadLocalStudy {
+public class ThreadLocalStudy_alone {
     private static final AtomicInteger nextId = new AtomicInteger(0);
 
     private static final ThreadLocal<Integer> threadId =
-            new ThreadLocal<Integer>() {
-                @Override
-                protected Integer initialValue() {
-                    System.out.println("here");
-                    return nextId.getAndIncrement();
-                }
-            };
-
-    public static int get() {
-        return threadId.get();
-    }
-
+            ThreadLocal.withInitial(() -> {
+                System.out.println("here");
+                return nextId.getAndIncrement();
+            });
 
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
@@ -35,5 +29,4 @@ public class ThreadLocalStudy {
             ).start();
         }
     }
-
 }
---
+++ src/main/java/com/ek/study/conCurrent/ThreadLocal_lifeCycle.java
@@ -0,0 +1,39 @@
+package com.ek.study.conCurrent;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 线程池使用threadLocal必须每次使用完之后清理，否则会会出问题的
+ *
+ * @author lazyman
+ * @version v1.0
+ * @date 2017/9/14
+ */
+public class ThreadLocal_lifeCycle {
+
+    public static final ThreadLocal<String> THREAD_LOCAL = new ThreadLocal<>();
+
+    public static ExecutorService executors = Executors.newFixedThreadPool(2);
+
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            executors.submit(() -> {
+
+                String s = THREAD_LOCAL.get();
+                if (s == null || s.equals("")) {
+                    System.out.println("init");
+                    THREAD_LOCAL.set("lazyman");
+                }
+
+                System.out.println("s: " + s);
+                THREAD_LOCAL.remove();
+            });
+
+        }
+
+    }
+
+}
