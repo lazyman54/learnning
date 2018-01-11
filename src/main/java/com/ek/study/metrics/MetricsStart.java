edd3a2259dca11bc2f401db746255789ab05d845
lazyman <lazymank54@gmail.com>
2018-01-11 21:17:46 +0800

    performance

---
+++ src/main/java/com/ek/study/metrics/MetricsStart.java
@@ -0,0 +1,39 @@
+package com.ek.study.metrics;
+
+import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.MetricRegistry;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author lazyman
+ * @version v1.0
+ * @date 2018/1/9
+ */
+public class MetricsStart {
+    static final MetricRegistry metrics = new MetricRegistry();
+
+    public static void main(String[] args) {
+
+        startReport();
+        Meter requests = metrics.meter("requests");
+        requests.mark();
+        wait5Seconds();
+    }
+
+    static void startReport() {
+        ConsoleReporter reporter = ConsoleReporter.forRegistry(metrics)
+                .convertRatesTo(TimeUnit.SECONDS)
+                .convertDurationsTo(TimeUnit.MILLISECONDS)
+                .build();
+        reporter.start(1, TimeUnit.SECONDS);
+    }
+
+    static void wait5Seconds() {
+        try {
+            Thread.sleep(5 * 1000);
+        } catch (InterruptedException e) {
+        }
+    }
+}
